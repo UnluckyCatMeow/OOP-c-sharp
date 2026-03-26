3e307c37834f604d499355768b4651fe9d652d7a
Dasha <st4646048@stud.duikt.edu.ua>
2026-03-26 17:41:51 +0200

    Task2, я індивідуальник))

---
+++ Modular1/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular1
+{
+    public class FileLogger
+    {
+       public MessagePublisher messagePublisher = new MessagePublisher();
+       public void Events()
+        {
+        }
+    }
+}
