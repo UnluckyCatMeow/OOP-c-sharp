1f419619ebd67946b3227391d116edffe742d963
Dasha <st4646048@stud.duikt.edu.ua>
2026-03-26 12:35:18 +0200

    Add new files for Modular1

---
+++ Modular1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Modular1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Task1.Run();
+        }
+    }
+}
---
+++ Modular1/Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular1
+{
+    public delegate string TextOperation(string input);
+    public static class Task1
+    {
+        public static void Run()
+        {
+            string inputFile = "textPD25.txt";
+            string outputFile = "resultPD25.txt";
+
+            File.WriteAllText(outputFile, string.Empty);
+
+            TextOperation toUpper = (text) => text.ToUpper();
+            TextOperation countChars = (text) => $"Кількість символів: {text.Length}";
+            TextOperation countWords = (text) => {
+                int count = text.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                return $"Кількість слів: {count}";
+            };
+
+            Console.WriteLine("Обробка файлу...");
+
+            ProcessFile(inputFile, outputFile, toUpper);
+            ProcessFile(inputFile, outputFile, countChars);
+            ProcessFile(inputFile, outputFile, countWords);
+
+            Console.WriteLine($"Результати записані у {outputFile}");
+        }
+        public static void ProcessFile(string sourcePath, string destPath, TextOperation operation)
+        {
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (StreamWriter sw = File.AppendText(destPath))
+            {
+                sw.WriteLine($"Виконання операції: {operation.Method.Name}");
+                foreach (string line in lines)
+                {
+                    string result = operation(line);
+                    sw.WriteLine(result);
+                }
+                sw.WriteLine();
+            }
+        }
+    }
+}
